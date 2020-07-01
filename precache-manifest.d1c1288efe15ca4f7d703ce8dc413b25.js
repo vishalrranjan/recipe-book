1cf18e6067a6e57426f9041061278dcd09b96fcd
vishalrranjan <vishalkr275@gmail.com>
2020-07-01 21:01:53 +0530

    Updates

---
+++ precache-manifest.d1c1288efe15ca4f7d703ce8dc413b25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a2a053bebbcc896f9dfcf1bf7913c0",
+    "url": "/recipe-book/index.html"
+  },
+  {
+    "revision": "a50113981684b37a855d",
+    "url": "/recipe-book/static/css/main.cdbb368d.chunk.css"
+  },
+  {
+    "revision": "c17487ceb8265e070a4a",
+    "url": "/recipe-book/static/js/2.58a31ad7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/recipe-book/static/js/2.58a31ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50113981684b37a855d",
+    "url": "/recipe-book/static/js/main.a98a6078.chunk.js"
+  },
+  {
+    "revision": "bc080b6ba01eef4b319d",
+    "url": "/recipe-book/static/js/runtime-main.52f5807f.js"
+  }
+]);
